477c8440741c3dbd328704791c2054551556525c
Ben Alexander <ben@tuhax.com>
2022-05-11 10:16:57 -0500

    Update plugins&themes.sh

--- plugins&themes.sh
+++ plugins&themes.sh
@@ -2,9 +2,8 @@
 #run this script to copy initial useful plugins and themes. Must be run from the root directory of your obsidian vault.
 
 if ! [ ls -d .obsidian/plugins ] ; # check that we are in the right directory
-  then
-  echo "obsidian plugin folder not found. Exiting."
-  exit(1)
+  then; echo "obsidian plugin folder not found. Exiting."
+    exit(1)
 fi
 cp -r env/obsidian/plugins/* .obsidian/plugins/
 mkdir .obsidian/plugins/themes
